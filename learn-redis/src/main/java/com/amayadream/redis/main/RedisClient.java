c927c7930491a28b5dbd0f0f20ea58e985c68f14
Amayadream <524806599@qq.com>
2016-07-26 22:17:56 +0800

    添加java操作redis的示例

---
+++ learn-redis/src/main/java/com/amayadream/redis/main/RedisClient.java
@@ -0,0 +1,165 @@
+package com.amayadream.redis.main;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.ResourceBundle;
+
+/**
+ * @author :  Amayadream
+ * @date :  2016.07.26 21:31
+ */
+public class RedisClient {
+
+    public static JedisPool jedisPool;
+
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle("properties/config");
+        int maxActive = Integer.parseInt(bundle.getString("redis.pool.maxActive"));
+        int maxIdle = Integer.parseInt(bundle.getString("redis.pool.maxIdle"));
+        int maxWait = Integer.parseInt(bundle.getString("redis.pool.maxWait"));
+
+        String ip = bundle.getString("redis.ip");
+        int port = Integer.parseInt(bundle.getString("redis.port"));
+
+        JedisPoolConfig config = new JedisPoolConfig();
+        //设置最大连接数
+        config.setMaxTotal(maxActive);
+        //设置最大空闲数
+        config.setMaxIdle(maxIdle);
+        //设置超时时间
+        config.setMaxWaitMillis(maxWait);
+
+        //初始化连接池
+        jedisPool = new JedisPool(config, ip, port);
+    }
+
+    /**
+     * 向缓存中设置字符串内容
+     * @param key
+     * @param value
+     * @return
+     */
+    public static boolean set(String key, String value) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            jedis.set(key, value);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+            jedisPool.close();
+        }
+    }
+
+    /**
+     * 向缓存里设置对象
+     * @param key
+     * @param value
+     * @return
+     */
+    public static boolean set(String key, Object value) {
+        Jedis jedis = null;
+        try {
+            String objectJson = JSON.toJSONString(value);
+            jedis = jedisPool.getResource();
+            jedis.set(key, objectJson);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+            jedisPool.close();
+        }
+    }
+
+    /**
+     * 删除缓存中得对象，根据key
+     * @param key
+     * @return
+     */
+    public static boolean del(String key){
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            jedis.del(key);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }finally{
+            if (jedis != null) {
+                jedis.close();
+            }
+            jedisPool.close();
+        }
+    }
+
+    /**
+     * 根据key 获取内容
+     * @param key
+     * @return
+     */
+    public static Object get(String key){
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            Object value = jedis.get(key);
+            return value;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }finally{
+            if (jedis != null) {
+                jedis.close();
+            }
+            jedisPool.close();
+        }
+    }
+
+
+    /**
+     * 根据key 获取对象
+     * @param key
+     * @return
+     */
+    public static <T> T get(String key,Class<T> clazz){
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            String value = jedis.get(key);
+            return JSON.parseObject(value, clazz);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }finally{
+            if (jedis != null) {
+                jedis.close();
+            }
+            jedisPool.close();
+        }
+    }
+
+    @Test
+    public void test(){
+        String s = "hello, world";
+//        set("Amayadream", s);
+        SimpleObject object = new SimpleObject("Amayadream", "hello, Amayadream");
+//        set("message", object);
+        object = null;
+        object = get("message", SimpleObject.class);
+        System.out.println(JSON.toJSONString(object));
+    }
+
+}
---
+++ learn-redis/src/main/java/com/amayadream/redis/main/SimpleObject.java
@@ -0,0 +1,35 @@
+package com.amayadream.redis.main;
+
+/**
+ * @author :  Amayadream
+ * @date :  2016.07.26 22:06
+ */
+public class SimpleObject {
+
+    private String name;
+    private String message;
+
+    public SimpleObject(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public SimpleObject() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
