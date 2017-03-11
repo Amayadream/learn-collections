7259940de810686414a734d80b7ed9467b7ab508
Amayadream <524806599@qq.com>
2017-03-11 20:46:51 +0800

    完善调度逻辑

--- learn-spring-quartz/src/main/java/com/amayadream/quartz/SpringStartListener.java
+++ learn-spring-quartz/src/main/java/com/amayadream/quartz/SpringStartListener.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -18,7 +17,6 @@ import java.util.List;
  * @author :  Amayadream
  * @date :  2016.12.19 22:12
  */
-@Component
 public class SpringStartListener implements ApplicationListener<ContextRefreshedEvent> {
 
     @Autowired(required = false)
@@ -29,33 +27,31 @@ public class SpringStartListener implements ApplicationListener<ContextRefreshed
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
-        if(event.getApplicationContext().getParent() == null){
-            Scheduler scheduler = schedulerFactoryBean.getScheduler();
-            List<ScheduleJob> jobs = schedulerService.list();
-            for (ScheduleJob job : jobs) {
-                if ("1".equals(job.getJobStatus())){    //初始化正常的任务
-                    TriggerKey triggerKey = TriggerKey.triggerKey(job.getJobName(), job.getJobGroup());
-                    try {
-                        CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);   //获取trigger
-                        if (null == trigger) {  //如果Trigger不存在, 则创建
-                            JobDetail jobDetail = JobBuilder.newJob(QuartzJobFactory.class).withIdentity(job.getJobName(), job.getJobGroup()).build();
-                            jobDetail.getJobDataMap().put("scheduleJob", job);
-                            //表达式调度构建器
-                            CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(job.getCronExpression());
-                            //按新的cronExpression表达式构建一个新的trigger
-                            trigger = TriggerBuilder.newTrigger().withIdentity(job.getJobName(), job.getJobGroup()).withSchedule(scheduleBuilder).build();
-                            scheduler.scheduleJob(jobDetail, trigger);
-                        } else {    //如果Trigger已存在, 则更新
-                            //表达式调度构建器
-                            CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(job.getCronExpression());
-                            //按新的cronExpression表达式重新构建trigger
-                            trigger = trigger.getTriggerBuilder().withIdentity(triggerKey).withSchedule(scheduleBuilder).build();
-                            //按新的trigger重新设置job执行
-                            scheduler.rescheduleJob(triggerKey, trigger);
-                        }
-                    } catch (SchedulerException e) {
-                        e.printStackTrace();
+        Scheduler scheduler = schedulerFactoryBean.getScheduler();
+        List<ScheduleJob> jobs = schedulerService.list();
+        for (ScheduleJob job : jobs) {
+            if ("1".equals(job.getJobStatus())){    //初始化正常的任务
+                TriggerKey triggerKey = TriggerKey.triggerKey(job.getJobName(), job.getJobGroup());
+                try {
+                    CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);   //获取trigger
+                    if (null == trigger) {  //如果Trigger不存在, 则创建
+                        JobDetail jobDetail = JobBuilder.newJob(QuartzJobFactory.class).withIdentity(job.getJobName(), job.getJobGroup()).build();
+                        jobDetail.getJobDataMap().put("scheduleJob", job);
+                        //表达式调度构建器
+                        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(job.getCronExpression());
+                        //按新的cronExpression表达式构建一个新的trigger
+                        trigger = TriggerBuilder.newTrigger().withIdentity(job.getJobName(), job.getJobGroup()).withSchedule(scheduleBuilder).build();
+                        scheduler.scheduleJob(jobDetail, trigger);
+                    } else {    //如果Trigger已存在, 则更新
+                        //表达式调度构建器
+                        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(job.getCronExpression());
+                        //按新的cronExpression表达式重新构建trigger
+                        trigger = trigger.getTriggerBuilder().withIdentity(triggerKey).withSchedule(scheduleBuilder).build();
+                        //按新的trigger重新设置job执行
+                        scheduler.rescheduleJob(triggerKey, trigger);
                     }
+                } catch (SchedulerException e) {
+                    e.printStackTrace();
                 }
             }
         }
--- learn-spring-quartz/src/main/java/com/amayadream/quartz/controller/QuartzController.java
+++ learn-spring-quartz/src/main/java/com/amayadream/quartz/controller/QuartzController.java
@@ -101,30 +101,35 @@ public class QuartzController {
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public String update(ScheduleJob job, RedirectAttributes attributes) throws SchedulerException {
         Scheduler scheduler = schedulerFactoryBean.getScheduler();
-        //获取触发器标识
-        TriggerKey triggerKey = TriggerKey.triggerKey(job.getJobName(), job.getJobGroup());
-        //获取触发器trigger
-        CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
-        if (trigger == null) {//不存在任务
-            attributes.addFlashAttribute("error", "任务不存在, 请确认后再操作!");
-        } else {    //Trigger已存在，那么更新相应的定时设置
-            //表达式调度构建器
-            CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(job.getCronExpression());
-            //按新的cronExpression表达式重新构建trigger
-            trigger = trigger.getTriggerBuilder()
-                    .withIdentity(triggerKey)
-                    .withSchedule(scheduleBuilder)
-                    .build();
-            scheduler.rescheduleJob(triggerKey, trigger);   //按新的trigger重新设置job执行
-            if ("0".equals(job.getJobStatus())){
-                scheduler.pauseTrigger(triggerKey);     //暂停状态
+        ScheduleJob j = schedulerService.find(job.getJobId());
+        if (j == null) {
+            attributes.addFlashAttribute("error", "数据库中没有找到该任务!");
+        } else {
+            //获取触发器标识
+            TriggerKey triggerKey = TriggerKey.triggerKey(j.getJobName(), j.getJobGroup());
+            //获取触发器trigger
+            CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
+            if (trigger == null) {  //TODO 如果队列里没有, 应该直接加入
+                attributes.addFlashAttribute("error", "任务不存在, 请确认后再操作!");
+            } else {    //Trigger已存在，那么更新相应的定时设置
+                //表达式调度构建器
+                CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(job.getCronExpression());
+                //按新的cronExpression表达式重新构建trigger
+                trigger = trigger.getTriggerBuilder()
+                        .withIdentity(triggerKey)
+                        .withSchedule(scheduleBuilder)
+                        .build();
+                scheduler.rescheduleJob(triggerKey, trigger);   //按新的trigger重新设置job执行
+                if ("0".equals(job.getJobStatus())){
+                    scheduler.pauseTrigger(triggerKey);     //暂停状态
+                }
+                //更新数据库中的任务
+                int result = schedulerService.update(job);
+                if (result > 0)
+                    attributes.addFlashAttribute("message", "任务[" + job.getJobName() + "]已更新!");
+                else
+                    attributes.addFlashAttribute("error", "任务[" + job.getJobName() + "]更新出错, 请重新尝试!");
             }
-            //更新数据库中的任务
-            int result = schedulerService.update(job);
-            if (result > 0)
-                attributes.addFlashAttribute("message", "任务[" + job.getJobName() + "]已更新!");
-            else
-                attributes.addFlashAttribute("error", "任务[" + job.getJobName() + "]更新出错, 请重新尝试!");
         }
         return "redirect:/quartz";
     }
@@ -144,7 +149,23 @@ public class QuartzController {
             //获取触发器trigger
             CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
             if (trigger == null) {  //TODO 如果队列里没有, 应该直接加入并暂停
-                attributes.addFlashAttribute("error", "任务队列中没有找到该任务!");
+                JobDetail jobDetail = JobBuilder.newJob(QuartzJobFactory.class)
+                        .withIdentity(job.getJobName(), job.getJobGroup())
+                        .build();
+                jobDetail.getJobDataMap().put("scheduleJob", job);
+                //表达式调度构建器
+                CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(job
+                        .getCronExpression());
+                //按新的cronExpression表达式构建一个新的trigger
+                trigger = TriggerBuilder.newTrigger()
+                        .withIdentity(job.getJobName(), job.getJobGroup())
+                        .withSchedule(scheduleBuilder)
+                        .build();
+                scheduler.scheduleJob(jobDetail, trigger);
+                scheduler.pauseTrigger(triggerKey);     //默认暂停状态
+                job.setJobStatus("0");
+                schedulerService.update(job);
+                attributes.addFlashAttribute("message", "检测到当前队列里没有任务[" + job.getJobName() + "], 已自动加入并暂停!");
             } else {
                 JobKey jobKey = JobKey.jobKey(job.getJobName(), job.getJobGroup());
                 scheduler.pauseJob(jobKey);
@@ -171,7 +192,21 @@ public class QuartzController {
             //获取触发器trigger
             CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
             if (trigger == null) { //TODO 如果队列里没有, 应该直接加入
-                attributes.addFlashAttribute("error", "任务队列中没有找到该任务!");
+                JobDetail jobDetail = JobBuilder.newJob(QuartzJobFactory.class)
+                        .withIdentity(job.getJobName(), job.getJobGroup())
+                        .build();
+                jobDetail.getJobDataMap().put("scheduleJob", job);
+                //表达式调度构建器
+                CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(job.getCronExpression());
+                //按新的cronExpression表达式构建一个新的trigger
+                trigger = TriggerBuilder.newTrigger()
+                        .withIdentity(job.getJobName(), job.getJobGroup())
+                        .withSchedule(scheduleBuilder)
+                        .build();
+                scheduler.scheduleJob(jobDetail, trigger);
+                job.setJobStatus("1");
+                schedulerService.update(job);
+                attributes.addFlashAttribute("message", "检测到当前队列里没有任务[" + job.getJobName() + "], 已自动加入!");
             } else {
                 JobKey jobKey = JobKey.jobKey(job.getJobName(), job.getJobGroup());
                 scheduler.resumeJob(jobKey);
